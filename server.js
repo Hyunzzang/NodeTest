d1198773604949aa5f0be05318337bd7436f955b
박현철 <phc023@gmail.com>
2019-08-22 18:46:42 +0900

    apm test

--- server.js
+++ server.js
@@ -4,11 +4,35 @@ var router = require('./router/main')(app);
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
 
+// elastic-apm-node
+const apm = require('elastic-apm-node').start({
+    serviceName: 'NodeTest',
+    secretToken: '',
+    serverUrl: 'http://localhost:8200',
+  })
+
+// Error handler
+function onError(err) {
+    if (apm.active)
+      apm.captureError(err);
+}
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public'));
-app.use(cookieParser);
+app.use(cookieParser());
+app.on('error', onError);
+app.get('/test', function (req, res) {
+    var trans = apm.startTransaction('test-1', 'test');
+    trans.result = "ok";
+    trans.end();
+    res.send('hi!')
+})
+
+app.get('/error', function (req, res) {
+    throw new Error('Broke!');
+})
 
 //if ('development' == app.get('env')) {
 //    app.use(express.errorHandler());
@@ -36,19 +60,21 @@ server.on('close', function () {
     console.log("Close On.");
 });
 
+server.on('error', onError);
+
 /**
  * socket.io
  */
-var server = require('http').createServer();
-var io = require('socket.io')(server);
-io.on('connection', function (client) {
-    console.log("socket.io - connection.");
+// var socketServer = require('http').createServer();
+// var io = require('socket.io')(socketServer);
+// io.on('connection', function (client) {
+//     console.log("socket.io - connection.");
 
-    client.on('event', function (data) { 
-        console.log("socket.io - event.");
-    });
-    client.on('disconnect', function () { 
-        console.log("socket.io - disconnect.");
-    });
-});
-server.listen(3000);
+//     client.on('event', function (data) { 
+//         console.log("socket.io - event.");
+//     });
+//     client.on('disconnect', function () { 
+//         console.log("socket.io - disconnect.");
+//     });
+// });
+// socketServer.listen(3000);
